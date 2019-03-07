e02e27e46e888369cbd2691491995f409f345e00
18601949127 <1991201740@qq.com>
2019-03-07 20:11:06 +0800

    添加百度地图 加上拥挤度显示

---
+++ app/src/main/java/com/tantuo/didicar/MainActivity.java
@@ -0,0 +1,48 @@
+package com.tantuo.didicar;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+    private BaiduMap mBaiduMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
+        setContentView(R.layout.activity_main);
+
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+        mMapView.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+        mMapView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+    }
+
+}
